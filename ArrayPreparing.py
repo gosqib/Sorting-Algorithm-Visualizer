3a7904624cf8ac99691582c0c429090b009ba293
a name <69024184+gosqib@users.noreply.github.com>
2021-09-04 20:56:18 -0400

    Update ArrayPreparing.py

--- ArrayPreparing.py
+++ ArrayPreparing.py
@@ -1,69 +1,12 @@
-from typing import Any, Callable, List, Tuple, Union
+from typing import Any, Callable, List, Optional, Tuple, Union
 from General import *
-import pygame
 import random
-pygame.init()
-
-# repeat of main loop
-# used to reshow ui when refill calls a blank screen
-def still_frame() -> None:
-	pygame.draw.rect(DISPLAY, RED, pygame.Rect(0, 100, 250, SCREEN_HEIGHT - 100))
-		
-	Button("New Array", 24).button(
-		30, 200, 
-		190, 50, 
-		(0, 190, 0), (0, 190, 0), 
-		)
-
-	pygame.draw.rect(DISPLAY, BLACK, pygame.Rect(0, 0, SCREEN_WIDTH, 100))
-	Text("Pygame Sorting Algorithm Visualizer", YELLOW, (500, 30), 40).display()
-	
-	MultipleLine(
-		["Welcome to the Pygame", "Algorithm Visualizer. ", "Press a button to start."], 
-		BLUE, (10, 115), 23).display()
-		
-	Button("Merge Sort", 24).button(
-		30, 350, 
-		190, 50, 
-		(0, 190, 0), (0, 255, 0), 
-		)
-
-	Button("Insertion Sort", 24).button(
-		30, 400, 
-		190, 50, 
-		(0, 190, 0), (0, 255, 0), 
-		)
-
-	Button("Quicksort", 24).button(
-		30, 450, 
-		190, 50, 
-		(0, 190, 0), (0, 255, 0), 
-		)
-
-	Button("Heap Sort", 24).button(
-		30, 500, 
-		190, 50, 
-		(0, 190, 0), (0, 255, 0), 
-		)
-
-	Button("Bubble Sort", 24).button(
-		30, 550, 
-		190, 50, 
-		(0, 190, 0), (0, 255, 0), 
-		)
-		
-	Button("Exit Program", 24).button(
-		30, 700, 
-		190, 50, 
-		(0, 190, 0), (0, 255, 0), 
-		)
 
 
 
 class Unsorted:
-	# done as default to avoid creating new placeholder for brand new arrays vs reworking with old ones
-	def __init__(self, unsort_arr: List[int] = [random.randint(5, SCREEN_HEIGHT - 105) for _ in range(150)]) -> None:
-		self.unsort_arr = unsort_arr
+	def __init__(self, unsort_arr: Optional[List[int]] = None):
+		self.unsort_arr = unsort_arr or [random.randint(5, SCREEN_HEIGHT - 105) for _ in range(150)]
 		self.col_arr = [GREEN] * 150
 
 	def create_arr(self) -> None:
@@ -90,3 +33,52 @@ class Unsorted:
 
 
 
+# repeat of main loop
+# used to reshow ui when refilling to show algorithm process later on calls for a blank screen
+def still_frame() -> None:
+	pygame.draw.rect(DISPLAY, (0, 116, 116), pygame.Rect(0, 100, 250, SCREEN_HEIGHT - 100))
+		
+	Button("New Array", 24).button(
+		30, 200, 
+		190, 50, 
+		(0, 190, 0), (0, 190, 0), )
+
+	pygame.draw.rect(DISPLAY, (0, 33, 71), pygame.Rect(0, 0, SCREEN_WIDTH, 100))
+	Text("Pygame Sorting Algorithm Visualizer", (28, 169, 201), (500, 30), 40).display()
+	
+	MultipleLine(
+		["Welcome to the Pygame", "Algorithm Visualizer. ", "Press a button to start."], 
+		(143, 188, 143), (10, 115), 23).display()
+		
+	Button("Merge Sort", 24).button(
+		30, 350, 
+		190, 50, 
+		(0, 190, 0), (0, 255, 0), )
+
+	Button("Insertion Sort", 24).button(
+		30, 400, 
+		190, 50, 
+		(0, 190, 0), (0, 255, 0), )
+
+	Button("Quicksort", 24).button(
+		30, 450, 
+		190, 50, 
+		(0, 190, 0), (0, 255, 0), )
+
+	Button("Heap Sort", 24).button(
+		30, 500, 
+		190, 50, 
+		(0, 190, 0), (0, 255, 0), )
+
+	Button("Bubble Sort", 24).button(
+		30, 550, 
+		190, 50, 
+		(0, 190, 0), (0, 255, 0), )
+		
+	Button("Exit Program", 24).button(
+		30, 700, 
+		190, 50, 
+		(0, 190, 0), (0, 255, 0), )
+
+
+		
